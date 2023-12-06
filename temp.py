717c7321b232d4a99a4072ea5a263eff9a8bea10
Rezsek Máté <rezsekmv@gmail.com>
2023-12-06 06:25:49 +0100

    day 6

--- temp.py
+++ temp.py
@@ -1,10 +1,12 @@
 import sys
 sys.path.append('../advent_of_code')
 from input_data import get_data
+import util
 import math
-from collections import deque
+from collections import deque, defaultdict
 
 text = get_data(1, 2023)
 data = text.split('\n')
 
-print(data)
+for d in data:
+    print(d)
