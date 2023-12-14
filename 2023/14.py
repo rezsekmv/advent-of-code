96478aad937a975f233259f2f63c4dd07b3a9668
Rezsek Máté <mate.vencel.rezsek@kh.hu>
2023-12-14 09:30:13 +0100

    clean day 14

--- 2023/14.py
+++ 2023/14.py
@@ -1,9 +1,7 @@
 import sys
 sys.path.append('../advent_of_code')
 from input_data import get_data
-import util
 from copy import deepcopy
-from collections import deque, defaultdict
 
 text = get_data(14, 2023)
 lines = text.split('\n')
@@ -18,31 +16,40 @@ for r in range(R):
         if G[r][c] == 'O':
             rocks.append((r,c))
 
-cache = []
-for cycle in range(1_000_000_000):
-    if G in cache:
-        break
-    cache.append(deepcopy(G))
-    for rr, cc in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
-        moved = True
-        while moved:
-            moved = False
-            for i, (r, c) in enumerate(rocks):
-                if 0 <= r+rr < R and 0 <= c+cc < C and G[r+rr][c+cc] == '.':
-                    G[r][c] = '.'
-                    G[r+rr][c+cc] = 'O'
-                    rocks[i] = (r+rr, c+cc)
-                    moved = True
+for p2 in [False, True]:
+    cache = []
+    for cycle in range(1_000_000_000):
+        if p2:
+            if G in cache:
+                break
+            cache.append(deepcopy(G))
+        for rr, cc in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+            moved = True
+            while moved:
+                moved = False
+                for i, (r, c) in enumerate(rocks):
+                    if 0 <= r+rr < R and 0 <= c+cc < C and G[r+rr][c+cc] == '.':
+                        G[r][c] = '.'
+                        G[r+rr][c+cc] = 'O'
+                        rocks[i] = (r+rr, c+cc)
+                        moved = True
 
-idx = cache.index(G)
-cache = cache[idx:]
-G = cache[(1_000_000_000-cycle) % len(cache)]
+            if not p2:
+                break
+        if not p2:
+            break        
+        
 
-p1 = 0
-for r in range(R):
-    for c in range(C):
-        if G[r][c] == 'O':
-            p1 += R-r
+    if p2:
+        idx = cache.index(G)
+        cache = cache[idx:]
+        G = cache[(1_000_000_000-cycle) % len(cache)]
 
-print(p1)      
+    ans = 0
+    for r in range(R):
+        for c in range(C):
+            if G[r][c] == 'O':
+                ans += R-r
+
+    print(ans)      
 
