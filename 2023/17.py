729e23d97666f9d1d3bd7dcffac44b58834a8b02
Rezsek Máté <rezsekmv@gmail.com>
2023-12-18 22:28:45 +0100

    17 improvements

--- 2023/17.py
+++ 2023/17.py
@@ -1,48 +1,82 @@
 import sys
 sys.path.append('../advent_of_code')
 from input_data import get_data
-from collections import deque
+from collections import deque, defaultdict
 import math
 
-text = get_data(17, 2023, True)
+text = get_data(17, 2023)
 lines = text.split('\n')
 
 R = len(lines)
 C = len(lines[0])
 G = [[int(lines[i][j]) for j in range(C)] for i in range(R)]
+
 class Node:
-    def __init__(self, coords, direction, distance, straight):
+    def __init__(self, coords, dist, wrong_dir):
         self.crd = coords
-        self.dir = direction
-        self.dist = distance
-        self.str = straight
+        self.dist = dist
+        self.wrong_dir = wrong_dir
     
     def __str__(self):
-        return '{} {} {} {}'.format(self.coords, self.direction, self.distance, self.straight)
+        return 'Node: {} {} {}'.format(self.crd, self.dist, self.wrong_dir)
+
+    def __eq__(self, other):
+        return self.crd == other.crd and self.dist == other.dist and self.wrong_dir == other.wrong_dir
+    
+    def __hash__(self) -> int:
+        return hash((self.crd, self.dist, self.wrong_dir))
 
 DIRECTIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-distances = [[(math.inf, 0) for j in range(C)] for i in range(R)]
 
-unvisited = [[[(i, j, k) for k in range(4)] for j in range(C)] for i in range(R)]
 
-buffer = deque([Node((0,0), 2, 0, 0)])
-while len(buffer) > 0:
-    node = buffer.popleft()
-
-    for d in range(node.dir-1, node.dir+2):
-        d %= len(DIRECTIONS)
-        r = node.crd[0]+DIRECTIONS[d][0]
-        c = node.crd[1]+DIRECTIONS[d][1]
-        straight = node.str+1 if d == node.dir else 1
-
-        if straight > 3:
-            continue
+def dijkstra():
+    dists = {}
+    visited = set()
+    
+    start = Node((0, 0), 0, -1)
+    buffer = deque([start])
+    while buffer:
+        curr = buffer.popleft()
         
-        if 0 <= r < R and 0 <= c < C:
-            if distances[r][c][0] < node.dist:
-                continue
+        # found the distance to bottom right
+        if curr.crd[0] == R-1 and curr.crd[1] == C-1:
+            return curr.dist
 
-            distances[r][c] = (node.dist+G[r][c], straight)
-            buffer.append(Node((r,c), d, node.dist+G[r][c], straight))
+        # if already visited => skip
+        tmp = Node(curr.crd, None, curr.wrong_dir)
+        if tmp in visited:
+            continue
+        visited.add(tmp)
+
+        # go trough neighbours in graph
+        for i, d in enumerate(DIRECTIONS):
+            # don't go backwards
+            if curr.dir == (i-2)%len(DIRECTIONS):
+                continue
             
-print(distances[R-1][C-1])
+            # if go straight => 
+            if curr.dir == i:
+                pass
+            
+            for distance in range(1, 4):
+                tmp_dist = 0
+                rr = curr.crd[0] + d[0] * distance
+                cc = curr.crd[1] + d[1] * distance
+                if 0<=rr<R and 0<=cc<C:
+                    tmp_dist += G[rr][cc]
+                    new_dist = curr.dist + tmp_dist
+                    Node((rr, cc), d)
+                    if dists.get() <= new_dist:
+                        continue
+                    dists[Node((rr, cc), d)] = new_dist
+                    buffer.append(Node((rr, cc), new_dist, d))
+
+print(dijkstra())
+
+# for i, (k,v) in enumerate(graph.items()):
+#     print(k)
+#     for g in v:
+#         print('\t', g)
+#     if i == 30:
+#         break
+# print(len(graph))
