bf728de333ecfec079b841ba707b089c17cf5be7
Rezsek Máté <mate.vencel.rezsek@kh.hu>
2023-12-19 12:31:04 +0100

    day 18 clean

--- 2023/18.py
+++ 2023/18.py
@@ -1,10 +1,8 @@
 import sys
 sys.path.append('../advent_of_code')
 from input_data import get_data
-from collections import deque
-import math
 
-text = get_data(18, 2023, True)
+text = get_data(18, 2023)
 lines = text.split('\n')
 
 DIR = {
@@ -21,29 +19,50 @@ DIR2 = {
     '2': (0, -1)
 }
 
-digged = set()
-current = (0, 0)
-for i, l in enumerate(lines):
-    print(i)
+def polygonArea(X, Y):
+    n = len(X)
+    area = 0.0
+ 
+    j = n - 1
+    for i in range(0,n):
+        area += (X[j] + X[i]) * (Y[j] - Y[i])
+        j = i
     
-    dir, dist, color = l.split()
-    dist = int(color[2:-2], 16)
-    print(dist)
-    dir = color[-2]
+    return int(abs(area / 2.0))
+ 
+current = (0, 0)
+side = 0
+X = [0]
+Y = [0]
 
-    next = (current[0] + DIR2[dir][0]*dist, current[1] + DIR2[dir][1]*dist)
-    digged.add((current, next))
+X2 = [0]
+Y2 = [0]
+current2 = (0, 0)
+side2 = 0
+for i, l in enumerate(lines):    
+    # part1
+    dir, dist, color = l.split()
+    dist = int(dist)
+
+    next = (current[0] + DIR[dir][0]*dist, current[1] + DIR[dir][1]*dist)
+    side += abs(next[0]-current[0]) + abs(next[1]-current[1])
+    X.append(next[0])
+    Y.append(next[1])
     current = next
 
-count = 0
-buffer = deque([(1, 1)])
-while len(buffer) > 0:
-    r, c = buffer.popleft()
-    for (rr, cc) in DIR.values():
-        next = (r+rr, c+cc)
-        if next not in digged:
-            digged.add(next)
-            buffer.append(next)
+    # part2
+    dist2 = int(color[2:-2], 16)
+    dir2 = color[-2]
+    next2 = (current2[0] + DIR2[dir2][0]*dist2, current2[1] + DIR2[dir2][1]*dist2)
+    side2 += abs(next2[0]-current2[0]) + abs(next2[1]-current2[1])
+    X2.append(next2[0])
+    Y2.append(next2[1])
+    current2 = next2
 
+side = side//2 + 1
+area = polygonArea(X, Y)
+print(area + side)
 
-print(len(digged))
+side2 = side2//2 + 1
+area2 = polygonArea(X2, Y2)
+print(area2+side2)
