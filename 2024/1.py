22a692d31682db38f9e83b2d80bb8e9fb42fb068
Rezsek Máté <mate.vencel.rezsek@kh.hu>
2024-12-01 08:20:27 +0100

    2024-1

--- 2024/1.py
+++ 2024/1.py
@@ -2,8 +2,14 @@ from input_data import get_data
 
 data = get_data(1).split()
 
-print(data)
+e = sorted(data[::2])
+m = sorted(data[1::2])
 
-p1 = 1
+p1 = 0
+p2 = 0
+for i in range(len(e)):
+    p1 += abs(int(e[i])-int(m[i]))
+    p2 += m.count(e[i])*int(e[i])
 
 print(p1)
+print(p2)
