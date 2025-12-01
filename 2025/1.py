820b3a8cc8ee0e245d8b338f11e566ee0dfa4c96
Máté Rezsek <rezsekmv.dev@gmail.com>
2025-12-01 06:57:47 +0100

    day 1

--- 2025/1.py
+++ 2025/1.py
@@ -1,10 +1,33 @@
 from util import *
 
-data = get_data(1).split()
+data = get_data(1, ex=False).split('\n')
 
 p1 = 0
 p2 = 0
+curr = 50
+
+def run(n, plus):
+    global curr, p2
+    for _ in range(n):
+        if plus:
+            curr += 1
+        else:
+            curr -= 1
+        curr = curr % 100
+        
+        if curr == 0:
+            p2 += 1
+
 for i,d in en(data):
-    pass
+    if (d[0] == 'R'):
+        run((int(d[1:])), True)
+    if (d[0] == 'L'):
+        run((int(d[1:])), False)
+    
+    if curr == 0:
+        p1 += 1
+    
+    curr = curr % 100
 
 pr(p1)
+pr(p2)
